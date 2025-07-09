7a16db63a21ec439664b820090cc83254f0c0c9c
SonicloverNINTENDEGARULES <o.oluwaloshe@gmail.com>
2025-07-08 20:13:11 -0500

    Add files via upload

---
+++ L4/Activity1.py
@@ -0,0 +1,10 @@
+a = 3
+b = 8
+c = 4
+d = 19
+e = 7 
+f = 12
+add = a + b + c + d + e + f
+print("The sum of the values is:", add)
+average = add / 6
+print("The average of the values is:", average)
---
+++ L4/Activty3.py
@@ -0,0 +1,15 @@
+
+print("Enter your percentage in the Subjects: ")
+math = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+spanish = int(input("spanish :"))
+
+
+sum = math+english+science+spanish
+print("sun of math,english,science and spanish")
+
+End_of_year_percentage = (sum/400)*100
+
+print(end="Percentage Mark = ")
+print(End_of_year_percentage)
