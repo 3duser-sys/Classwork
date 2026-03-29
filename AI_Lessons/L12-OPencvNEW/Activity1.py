5b7c502c9f3bd06f777e5a2b7b3dc4e33588751f
SonicloverNINTENDEGARULES <o.oluwaloshe@gmail.com>
2026-03-28 19:46:30 -0500

    Add files via upload

--- AI_Lessons/L12-OPencvNEW/Activity1.py
+++ AI_Lessons/L12-OPencvNEW/Activity1.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -16,26 +15,23 @@ while True:
     if not ret:
         print("Error: Could it be the camera? Couln't open it...\nError Code: E002RET")
         break
-    
+
     print("Detecting your face...")
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
     print("Face succesfully Detected!")
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    
-    cv2.imshow('Face Detection Started: Press q to quit', frame)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, f'Ppl Count: {len(faces)}', (10, 30), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+    cv2.imshow('Face Tracking AND FACE COUNTING!!!!', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 VidCap.release()
-cv2.destroyAllWindows()
-                      
-
-
-
-    
+cv2.destroyAllWindows
